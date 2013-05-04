796fbaf265e3eee79f63dd4f399de643646d24e1
Peter Johnson <peter@openshuffle.com>
2013-05-04 01:43:54 +0100

    formatting

--- netstat.js
+++ netstat.js
@@ -1,42 +1,44 @@
 var child = require('child_process'),
   util = require('util'),
   netstat = child.spawn('netstat', ['-nb']),
-  out = ''
- 
+  out = '';
+
 netstat.stdout.on('data', function(data) {
-  out += data
-})
+  out += data;
+});
+
 netstat.stderr.on('data', function(data) {
-  console.log('err: ' + data)
-})
+  console.log('err: ' + data);
+});
+
 netstat.on('exit', function(code) {
-  var i, bucket = [], processes = {}
+  var i, bucket = [], processes = {};
   if(code !== 0) {
-    console.log('!!! exited, status code: ' + code)
-    return
+    console.log('!!! exited, status code: ' + code);
+    return;
   }
- 
+
   // parse & serialize netstat output
-  out = out.split(/\n/)
+  out = out.split(/\n/);
   // drop the first three lines...
-  for(i = 0; i <= 3; i++) out.shift()
+  for(i = 0; i <= 3; i++) out.shift();
   out.forEach(function(entry) {
-    if(!entry) return
-    entry = entry.replace(/^\s\s*/, '').replace(/\r/, '').replace('Can not obtain ownership information', '[???]').split(/[ ]+/)
+    if(!entry) return;
+    entry = entry.replace(/^\s\s*/, '').replace(/\r/, '').replace('Can not obtain ownership information', '[???]').split(/[ ]+/);
     if(entry[0][0] == '[') {
-      i = entry[0].replace(/^\[([^\]]+)\]$/, '$1')
-      if(processes[i] === undefined) processes[i] = []
-      processes[i] = processes[i].concat(bucket)
-      bucket = []
+      i = entry[0].replace(/^\[([^\]]+)\]$/, '$1');
+      if(processes[i] === undefined) processes[i] = [];
+      processes[i] = processes[i].concat(bucket);
+      bucket = [];
     } else {
       bucket.push({
         conntype:entry[0],
         source:entry[1],
-        dest:entry[2],
+        dest:entry[2]
         //status:entry[3],
-      })
+      });
     }
-  })
-  out = JSON.stringify(processes)
+  });
+  out = JSON.stringify(processes);
   // READY FOR DISPATCH! *salute*
-})
+});
